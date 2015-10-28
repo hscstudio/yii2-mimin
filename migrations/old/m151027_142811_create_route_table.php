30c71b3581d4ead713a18c6dd93a686361605b6a
Hafid Mukhlasin <milisstudio@gmail.com>
2015-10-29 04:10:12 +0700

    Update m151027_142811_create_route_table.php

--- migrations/old/m151027_142811_create_route_table.php
+++ migrations/old/m151027_142811_create_route_table.php
@@ -7,34 +7,25 @@ class m151027_142811_create_route_table extends Migration
 {
     public function up()
     {
-		<?php
-
-use yii\db\Schema;
-use yii\db\Migration;
-
-class m151024_072453_create_route_table extends Migration
-{
-    public function up()
-    {
-		$tableOptions = null;
+	$tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 		
-		$this->createTable('{{%route}}', [
-			'name' => Schema::TYPE_STRING . '(64) NOT NULL',
-			'alias' => Schema::TYPE_STRING . '(64) NOT NULL',
-			'type' => Schema::TYPE_STRING . '(64) NOT NULL',
-			'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 1',
-			'PRIMARY KEY (name)'
+	$this->createTable('{{%route}}', [
+		'name' => Schema::TYPE_STRING . '(64) NOT NULL',
+		'alias' => Schema::TYPE_STRING . '(64) NOT NULL',
+		'type' => Schema::TYPE_STRING . '(64) NOT NULL',
+		'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 1',
+		'PRIMARY KEY (name)'
         ]), $tableOptions);
     }
 
     public function down()
     {
         echo "m151027_142811_create_route_table cannot be reverted.\n";
-		$this->dropTable('{{%route}}');
+	$this->dropTable('{{%route}}');
         return false;
     }
 
